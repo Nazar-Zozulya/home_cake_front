ab803fe6f88f203aeb3d618879d86c6d1d020e69
Nazar Zozulya <115177222+Nazar-Zozulya@users.noreply.github.com>
2025-07-17 15:37:55 +0300

    добавил футер

--- src/modules/layout/ui/footer/footer.tsx
+++ src/modules/layout/ui/footer/footer.tsx
@@ -1,10 +1,35 @@
-
-
-
+import { NavigationButton } from "../../../../ui/navigationButton"
+import styles from "./footer.module.css"
 
 export function Footer() {
-
-    return (
-        <div>footer</div>
-    )
+	return (
+		<div className={styles.container}>
+			<div className={styles.logo}>
+				<img
+					src="/assets/images/logo.png"
+					alt="Logo"
+					className={styles.logoImg}
+				/>
+				<p className={styles.logoText}>Home Cake</p>
+			</div>
+			<div className={styles.navigation}>
+                <NavigationButton title="Головна" onClick={() => {}} variant="small" />
+				<NavigationButton title="Новинки" onClick={() => {}} variant="small" />
+				<NavigationButton title="Наші переваги" onClick={() => {}} variant="small" />
+				<NavigationButton title="Асортимент" onClick={() => {}} variant="small" />
+				<NavigationButton title="Про нас" onClick={() => {}} variant="small" />
+			</div>
+            <div className={styles.contacts}>
+                <p className={styles.contactTitle}>Контакти</p>
+                <div className={styles.contactBlock}>
+                    {/* <img src="" alt="" className={styles.contactIcon} /> */}
+                    <p className={styles.contactText}>+380 999999999</p>
+                </div>
+                <div className={styles.contactBlock}>
+                    {/* <img src="" alt="" className={styles.contactIcon} /> */}
+                    <p className={styles.contactText}>home_cake@gmail.com</p>
+                </div>
+            </div>
+		</div>
+	)
 }
