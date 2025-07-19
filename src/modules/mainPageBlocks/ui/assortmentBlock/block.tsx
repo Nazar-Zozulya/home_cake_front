0276d9e6267e63eee3640800d3f6c5507a2abf8a
Nazar Zozulya <115177222+Nazar-Zozulya@users.noreply.github.com>
2025-07-19 22:10:11 +0300

    пофиксил отображение продуктов

--- src/modules/mainPageBlocks/ui/assortmentBlock/block.tsx
+++ src/modules/mainPageBlocks/ui/assortmentBlock/block.tsx
@@ -1,38 +1,12 @@
 import { ProductCard } from "../../../../components/productCard"
-import { Product } from "../../../product"
+import { Product, useProductContext } from "../../../product"
 import styles from "./block.module.css"
-
-const products: Product[] = [
-	{
-		id: 4,
-		name: "Торт Медовик",
-		price: 210,
-		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPIpP2bGDiALastuZaCJddEZX1eSGxAREL8Q&s",
-		composition: "Медові коржі, сметанний крем",
-		weight: 520,
-		kilocalories: 260,
-	},
-	{
-		id: 5,
-		name: "Торт Чізкейк",
-		price: 230,
-		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPIpP2bGDiALastuZaCJddEZX1eSGxAREL8Q&s",
-		composition: "Сирний крем, печиво, ягоди",
-		weight: 500,
-		kilocalories: 280,
-	},
-	{
-		id: 6,
-		name: "Торт Павлова",
-		price: 240,
-		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPIpP2bGDiALastuZaCJddEZX1eSGxAREL8Q&s",
-		composition: "Безе, вершковий крем, фрукти",
-		weight: 480,
-		kilocalories: 220,
-	},
-]
+import { sampleSize } from "lodash"
 
 export function AssortmentBlock() {
+	const { products } = useProductContext()
+	const randomProducts: Product[] = sampleSize(products, 3)
+
 	return (
 		<div className={styles.container}>
 			<img
@@ -43,20 +17,18 @@ export function AssortmentBlock() {
 			<p className={styles.title}>Наш асортимент</p>
 
 			<div className={styles.productsList}>
-				{products.map((product) => {
-					return (
-						<ProductCard
-							key={product.id}
-							id={product.id}
-							image={product.image}
-							name={product.name}
-							price={product.price}
-							composition={product.composition}
-							weight={product.weight}
-							kilocalories={product.kilocalories}
-						/>
-					)
-				})}
+				{randomProducts.map((product) => (
+					<ProductCard
+						key={product.id}
+						id={product.id}
+						image={product.image}
+						name={product.name}
+						price={product.price}
+						composition={product.composition}
+						weight={product.weight}
+						kilocalories={product.kilocalories}
+					/>
+				))}
 			</div>
 
 			<button className={styles.showAll}>Показати всі</button>
--- src/modules/mainPageBlocks/ui/newProductsBlock/block.tsx
+++ src/modules/mainPageBlocks/ui/newProductsBlock/block.tsx
@@ -1,45 +1,16 @@
 import { ProductCard } from "../../../../components/productCard"
-import { Product } from "../../../product"
+import { Product, useProductContext } from "../../../product"
 import styles from "./block.module.css"
 
-const products: Product[] = [
-	{
-		id: 4,
-		name: "Торт Медовик",
-		price: 210,
-		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPIpP2bGDiALastuZaCJddEZX1eSGxAREL8Q&s",
-		composition: "Медові коржі, сметанний крем",
-		weight: 520,
-		kilocalories: 260,
-	},
-	{
-		id: 5,
-		name: "Торт Чізкейк",
-		price: 230,
-		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPIpP2bGDiALastuZaCJddEZX1eSGxAREL8Q&s",
-		composition: "Сирний крем, печиво, ягоди",
-		weight: 500,
-		kilocalories: 280,
-	},
-	{
-		id: 6,
-		name: "Торт Павлова",
-		price: 240,
-		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPIpP2bGDiALastuZaCJddEZX1eSGxAREL8Q&s",
-		composition: "Безе, вершковий крем, фрукти",
-		weight: 480,
-		kilocalories: 220,
-	},
-]
-
-
 export function NewProductsBlock() {
+	const { products }  = useProductContext()
+
 	return (
 		<div className={styles.container}>
 			<img src="/assets/images/newProducts.jpg" alt="" className={styles.bg}  />
 			<p className={styles.title}>Наші Новинки</p>
 			<div className={styles.allProducts}>
-				{ products.map((product) => {
+				{ products.slice(-3).reverse().map((product) => {
 					return (
 						<ProductCard
 							key={product.id}
